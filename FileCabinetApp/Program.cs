d82eebf10b47db1049a2297a7512f48f6f201afe
l122 <minolyndo@gmail.com>
2023-01-23 16:24:10 +0500

    Change DeveloperName to Oleg Shkadov.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -4,7 +4,7 @@ namespace FileCabinetApp
 {
     public static class Program
     {
-        private const string DeveloperName = "Vasili Vasilyev2";
+        private const string DeveloperName = "Oleg Shkadov";
         private const string HintMessage = "Enter your command, or enter 'help' to get help.";
         private const int CommandHelpIndex = 0;
         private const int DescriptionHelpIndex = 1;
