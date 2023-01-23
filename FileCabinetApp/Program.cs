9dca5ca4cd314e12c53a605863318885dac526bb
l122 <minolyndo@gmail.com>
2023-01-23 16:19:22 +0500

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
