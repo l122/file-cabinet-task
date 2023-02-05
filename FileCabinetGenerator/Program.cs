468a031e5e5baf33b48f4ef918440beb1fe9de4b
l122 <minolyndo@gmail.com>
2023-02-05 14:05:28 +0500

    Create a new Console Application FileCabinetGenerator and add to FileCabinet.sln.

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+
+namespace FileCabinetGenerator
+{
+    /// <summary>
+    /// The class that contains the Main method.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
