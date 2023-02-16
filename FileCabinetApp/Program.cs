e1e56eedd2fa27cc8a87b6d35fad4487bbec01a1
l122 <minolyndo@gmail.com>
2023-02-16 16:10:53 +0500

    Implement the same argument parser for FileCabinetApp and FileCabinetGenerator. Add validator type argument for FileCabinetGenerator.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -191,7 +191,7 @@ namespace FileCabinetApp
         /// <param name="args">The <see cref="string"/> array instance input parameters.</param>
         private static void Init(string[] args)
         {
-            var parsedArgsDictionary = ParseArgs(args);
+            var parsedArgsDictionary = Parser.ParseArgs(args);
 
             int validatorIndex = 0;
             foreach (var flag in ValidationRulesFlags)
@@ -254,49 +254,6 @@ namespace FileCabinetApp
         private static IRecordValidator GetDefaultValidatorObject()
         {
             return new ValidatorBuilder().CreateDefaultValidator();
-        }
-
-        /// <summary>
-        /// Parses input arguments input a <see cref="Dictionary{TKey, TValue}"/>.
-        /// </summary>
-        /// <param name="args">The <see cref="string"/> array instance.</param>
-        /// <returns>The <see cref="Dictionary{TKey, TValue}"/> object.</returns>
-        private static Dictionary<string, string> ParseArgs(string[] args)
-        {
-            Dictionary<string, string> result = new ();
-            int i = 0;
-            while (i < args.Length)
-            {
-                args[i] = args[i].ToLower(CultureInfo.InvariantCulture);
-                if (args[i].StartsWith("--", StringComparison.InvariantCulture))
-                {
-                    var arg = args[i].Split("=");
-                    if (arg.Length >= 2)
-                    {
-                        result.Add(arg[0], arg[1]);
-                    }
-                    else
-                    {
-                        result.Add(arg[0], string.Empty);
-                    }
-                }
-                else if (args[i].StartsWith("-", StringComparison.InvariantCulture))
-                {
-                    if (i + 1 < args.Length && !args[i + 1].StartsWith("-", StringComparison.InvariantCulture))
-                    {
-                        result.Add(args[i], args[i + 1]);
-                        i++;
-                    }
-                    else
-                    {
-                        result.Add(args[i], string.Empty);
-                    }
-                }
-
-                i++;
-            }
-
-            return result;
         }
 
         private static void DefaultRecordPrint(IEnumerable<FileCabinetRecord> records)
---
+++ FileCabinetApp/StaticClasses/Parser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp.StaticClasses
+{
+    /// <summary>
+    /// Helper static class with parsing static functions.
+    /// </summary>
+    public static class Parser
+    {
+        /// <summary>
+        /// Parses input arguments input a <see cref="Dictionary{TKey, TValue}"/>.
+        /// </summary>
+        /// <param name="args">The <see cref="string"/> array instance.</param>
+        /// <returns>The <see cref="Dictionary{TKey, TValue}"/> object.</returns>
+        public static Dictionary<string, string> ParseArgs(string[] args)
+        {
+            Dictionary<string, string> result = new();
+            int i = 0;
+            while (i < args.Length)
+            {
+                args[i] = args[i].ToLower(CultureInfo.InvariantCulture);
+                if (args[i].StartsWith("--", StringComparison.InvariantCulture))
+                {
+                    var arg = args[i].Split("=");
+                    if (arg.Length >= 2)
+                    {
+                        result.Add(arg[0], arg[1]);
+                    }
+                    else
+                    {
+                        result.Add(arg[0], string.Empty);
+                    }
+                }
+                else if (args[i].StartsWith("-", StringComparison.InvariantCulture))
+                {
+                    if (i + 1 < args.Length && !args[i + 1].StartsWith("-", StringComparison.InvariantCulture))
+                    {
+                        result.Add(args[i], args[i + 1]);
+                        i++;
+                    }
+                    else
+                    {
+                        result.Add(args[i], string.Empty);
+                    }
+                }
+
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
