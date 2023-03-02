511ba5d9159dd56617acd5e97273c27c0d78df90
l122 <minolyndo@gmail.com>
2023-03-02 13:38:31 +0500

    Implement command hints.

--- FileCabinetApp/CommandHandlers/HelpCommandHandler.cs
+++ FileCabinetApp/CommandHandlers/HelpCommandHandler.cs
@@ -12,38 +12,6 @@ namespace FileCabinetApp.CommandHandlers
         private const int ExplanationHelpIndex = 2;
         private const string Trigger = "help";
 
-        private static readonly string[][] HelpMessages = new string[][]
-        {
-            new string[] { "create", "creates a new record", "The 'create' command creates a new record." },
-            new string[]
-            {
-                "delete",
-                "deletes records",
-                "The 'delete where <expression>' deletes the records that satisfy the expression."
-                + "\nThe expression should consist of record field names such as id, FirstName, LastName, DateOfBirth, Workplace, Salary, Department, "
-                + "as well as the logical and comparison operators such as 'and', 'or', 'not', '=' and '!='."
-                + "\nExampe: delete where id = 1 or firstname = johny and lastname != doe and not dateofbirth = 01.01.2000",
-            },
-            new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
-            new string[] { "export", "exports records to csv or xml", "The 'export <csv, xml> <file_name>' command exports records to a csv or xml file." },
-            new string[] { "find", "searches records", "The 'find <firstname, lastname, dateofbirth> <criterion>' command searches all records with <field> = <criterion>." },
-            new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
-            new string[] { "import", "imports records from csv or xml", "The 'import <csv, xml> <file_name>' command imports records from a csv or xml file." },
-            new string[] { "insert", "inserts a record", "The 'insert (id, firstname, lastname, dateofbirth, workplace, salary, department) values ('id#', 'firstname', 'lastname', 'dd.mm.yyyy', 'place#', 'salary', 'department')' command inserts a record." },
-            new string[] { "list", "prints all records", "The 'list' command prints all records." },
-            new string[] { "purge", "removes records marked as deleted from the database", "The 'purge' command removes records marked as deleted from the database." },
-            new string[] { "stat", "prints the number of records", "The 'stat' command prints the number of records." },
-            new string[]
-            {
-                "update",
-                "updates records",
-                "The 'update set field='value' where field='value' update the records that satisfy the where expression."
-                + "\nThe expression should consist of record field names such as id, FirstName, LastName, DateOfBirth, Workplace, Salary, Department, "
-                + "as well as the logical and comparison operators such as 'and', 'or', 'not', '=' and '!='."
-                + "\nExampe: update set firstname = John, lastname = Doe , dateofbirth = 01.01.2000 where id = 1",
-            },
-        };
-
         /// <inheritdoc/>
         public override void Handle(AppCommandRequest appCommandRequest)
         {
--- FileCabinetApp/CommandHandlers/MissedCommandHandler.cs
+++ FileCabinetApp/CommandHandlers/MissedCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FileCabinetApp.CommandHandlers
 {
@@ -10,8 +11,42 @@ namespace FileCabinetApp.CommandHandlers
         /// <inheritdoc/>
         public override void Handle(AppCommandRequest appCommandRequest)
         {
-            Console.WriteLine($"There is no '{appCommandRequest.Command}' command.");
+            Console.WriteLine($"There is no '{appCommandRequest.Command}' command. See 'help'.");
             Console.WriteLine();
+            var possibleCommands = GetSimilarCommands(appCommandRequest);
+            if (possibleCommands.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("The most similar commands are:");
+            foreach (var command in possibleCommands)
+            {
+                Console.WriteLine("\t\t{0}", command);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static List<string> GetSimilarCommands(AppCommandRequest request)
+        {
+            List<string> result = new ();
+
+            if (string.IsNullOrEmpty(request.Command))
+            {
+                return result;
+            }
+
+            foreach (var line in HelpMessages)
+            {
+                var command = line[0];
+                if (command.StartsWith(request.Command[0]) && request.Command.Length <= command.Length)
+                {
+                    result.Add(command);
+                }
+            }
+
+            return result;
         }
     }
 }
